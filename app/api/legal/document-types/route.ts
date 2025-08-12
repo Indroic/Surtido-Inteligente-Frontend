879ca305dc2e9861ff12885e72dc43e0557e5b33
David Latosefki <indroic@outlook.com>
2025-08-12 16:38:04 -0400

    añadido de rutas de impuestos y tipos de documentos

--- app/api/legal/document-types/route.ts
+++ app/api/legal/document-types/route.ts
@@ -2,14 +2,16 @@ import { NextRequest } from "next/server";
 import { getToken, JWT } from "next-auth/jwt";
 
 import { GenericRouteHandlers } from "@/app/api/GenericRouteHandlers";
-import { ImpuestoInterface } from "@/types/legal";
-import { ImpuestosAdapter } from "@/adapters/legal";
+import { DocumentTypeInterface } from "@/types/legal";
+import { DocumentsStypesAdapter } from "@/adapters/legal";
 
 const handlers: GenericRouteHandlers<
-  ImpuestoInterface,
-  Promise<ImpuestosAdapter>
+  DocumentTypeInterface,
+  Promise<DocumentsStypesAdapter>
 > = new GenericRouteHandlers((req: NextRequest) => {
-  return getToken({ req }).then((token) => new ImpuestosAdapter(token as JWT));
+  return getToken({ req }).then(
+    (token) => new DocumentsStypesAdapter(token as JWT),
+  );
 });
 
 export const { GET, POST, PUT, DELETE } = handlers.getAllHandlers();
